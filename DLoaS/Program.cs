be1426b733d71884a2e43fd41c74136ea5fde9c0
Fabian Schöffmann <fabian.schoeffmann@infineon.com>
2023-05-23 11:27:42 +0200

    Changed to 2023

--- DLoaS/Program.cs
+++ DLoaS/Program.cs
@@ -13,7 +13,7 @@ namespace DLoaS {
         static Program(){
             CosturaUtility.Initialize();
         }
-        static string year = "2022";
+        static string year = "2023";
 
         //static string[] d = new string[153];        //Anlegen der Link-Liste mit URLS
         static string[] d = dlInitA();
@@ -28,7 +28,8 @@ namespace DLoaS {
         static int[] michi;
         static int[] valentin;
         static int[] miri;
-        static int[] volker;
+        //static int[] volker;
+        static int[] dan;
         //static int[] rene;
 
         //Countervariablen der Teilnehmer
@@ -39,7 +40,8 @@ namespace DLoaS {
         static int michiScore = 0;
         static int valentinScore = 0;
         static int miriScore = 0;
-        static int volkerScore = 0;
+        //static int volkerScore = 0;
+        static int danScore = 0;
         //static int reneScore = 0;
 
         static void Main(string[] args) {
@@ -47,15 +49,16 @@ namespace DLoaS {
             //dlInit();                               //Befüllen der Link-Liste
 
             //Befüllen der Deathlists
-            emil        = new int[] {0,1,2,12,13,17,19,42,55,57,60,62,78,79,91,93,96,97,103,108,111,120,125,126,127,135,136,138,139,142};
-            fabian      = new int[] {0,2,3,18,25,31,35,45,46,47,60,62,69,84,90,92,96,97,103,107,116,117,120,120,124,125,126,127,138,139};
-            jasi        = new int[] {6,8,9,13,16,26,29,30,32,34,36,38,48,52,53,80,85,86,87,88,89,94,97,98,112,120,123,136,138,146};
-            manu        = new int[] {3,7,10,26,34,36,37,38,39,48,64,67,70,71,79,85,86,89,97,98,99,100,103,115,118,121,124,129,137,143};
-            michi       = new int[] {19,21,26,28,33,34,44,48,58,64,72,73,86,88,89,95,97,102,103,104,106,118,119,122,132,136,137,138,139,145};
+            emil        = new int[] { 0, 1, 2, 6, 12, 45, 59, 61, 66, 75, 77, 82, 84, 85, 97, 102, 111, 115, 121, 125, 129, 132, 133, 134, 138, 142, 143, 148, 149, 154 };
+            fabian      = new int[] { 0, 2, 3, 12, 15, 22, 33, 6, 40, 44, 49, 50, 51, 55, 66, 74, 85, 91, 99, 102, 110, 119, 120, 124, 125, 130, 132, 133, 134, 142 };
+            jasi        = new int[] { 4, 8, 13, 24, 30, 31, 34, 35, 37, 39, 52, 56, 57, 62, 86, 92, 93, 94, 95, 96, 100, 103, 108, 116, 125, 128, 142, 143, 151, 156 };
+            manu        = new int[] { 5, 9, 17, 23, 24, 26, 39, 41, 42, 52, 63, 69, 76, 77, 85, 95, 93, 96, 103, 104, 105, 118, 121, 122, 125, 130, 136, 143, 144, 145 };
+            michi       = new int[] { 17, 23, 24, 26, 27, 28, 36, 47, 52, 63, 66, 69, 77, 78, 79, 93, 95, 96, 101, 106, 109, 113, 121, 122, 123, 126, 141, 143, 144, 145 };
             //rene        = new int[] { 4, 13, 21, 23, 28, 36, 44, 49, 52, 55, 57, 58, 59, 63, 65, 72, 78, 87, 87, 96, 103, 105, 109, 118, 119, 120, 121, 133, 139, 146 };
-            valentin    = new int[] {6,13,14,19,24,27,29,43,50,59,66,67,77,83,85,87,88,98,101,105,38,111,112,128,131,134,141,144,147,148};
-            miri    = new int[] {11,16,22,23,26,33,36,48,49,52,54,54,56,63,67,74,76,81,82,85,89,107,109,110,112,113,123,144,146,148};
-            volker    = new int[] {4,5,13,15,20,33,35,40,41,46,51,60,61,65,68,75,88,97,103,107,114,117,126,127,130,133,136,138,140,148};
+            valentin    = new int[] { 4, 7, 14, 16, 21, 25, 29, 30, 46, 48, 64, 70, 71, 72, 83, 89, 92, 94, 95, 98, 103, 107, 115, 116, 137, 147, 150, 151, 152, 155 };
+            miri    = new int[] { 11, 13, 18, 19, 24, 36, 39, 53, 56, 58, 58, 60, 68, 71, 80, 81, 88, 92, 96, 110, 112, 114, 116, 117, 124, 128, 146, 150, 151, 155 };
+            //volker    = new int[] {4,5,13,15,20,33,35,40,41,46,51,60,61,65,68,75,88,97,103,107,114,117,126,127,130,133,136,138,140,148};
+            dan    = new int[] { 10, 12, 13, 19, 20, 24, 32, 37, 38, 42, 43, 54, 65, 67, 73, 84, 87, 90, 103, 104, 107, 36, 125, 127, 131, 135, 139, 140, 151, 153 };
 
 
             for (int i = 0; i<d.Length;i++) {                                                                           //Jeden Link im Array durchgehen
@@ -110,7 +113,8 @@ namespace DLoaS {
             //reneScore = fastSearch(dl, rene);
             valentinScore = fastSearch(dl, valentin);
             miriScore = fastSearch(dl, miri);
-            volkerScore = fastSearch(dl, volker);
+            //volkerScore = fastSearch(dl, volker);
+            danScore = fastSearch(dl, dan);
             
 
             //Console-Output mit allen Teilnehmern und deren Punkten
@@ -122,7 +126,8 @@ namespace DLoaS {
             //Console.WriteLine("Rene: " + reneScore);
             Console.WriteLine("Valentin: " + valentinScore);
             Console.WriteLine("Miri: " + miriScore);
-            Console.WriteLine("Volker: " + volkerScore);
+            //Console.WriteLine("Volker: " + volkerScore);
+            Console.WriteLine("Dan: " + danScore);
             
 
             string[] pD = { };
@@ -161,7 +166,8 @@ namespace DLoaS {
                 //whoPointed += pointSearch(Int32.Parse(oDiff), rene, "Rene ");
                 whoPointed += pointSearch(Int32.Parse(oDiff), valentin, "Valentin ");
                 whoPointed += pointSearch(Int32.Parse(oDiff), miri, "Miri ");
-                whoPointed += pointSearch(Int32.Parse(oDiff), volker, "Volker ");
+                //whoPointed += pointSearch(Int32.Parse(oDiff), volker, "Volker ");
+                whoPointed += pointSearch(Int32.Parse(oDiff), dan, "Dan ");
                 
 
                 //Client erstellen
